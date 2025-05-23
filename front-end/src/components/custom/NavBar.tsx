b8768c395d6796c122092f45d0f91a5f3c1afa65
HugoBraj <hugobrajdic@gmail.com>
2025-05-23 11:55:57 +0200

    update navbar

--- front-end/src/components/custom/NavBar.tsx
+++ front-end/src/components/custom/NavBar.tsx
@@ -1,41 +1,32 @@
 import { Folder, PackagePlus, ScanBarcode } from "lucide-react";
 import { NavLink } from "react-router-dom";
+import { Menubar, MenubarMenu, MenubarTrigger } from "../ui/menubar";
 
-const Ajouter = () => {
-    return <>
-        <div className='fixed bottom-0 left-0 right-0 h-16 flex justify-around'>
-            <NavLink className={({ isActive }) =>
-                isActive
-                    ? "flex-1 bg-sky-400 "
-                    : "flex-1 hover:bg-gray-400"
-            }
-                key="/inventaire"
-                to="/inventaire"
-                end>
-                <Folder />
-            </NavLink>
-            <NavLink className={({ isActive }) =>
-                isActive
-                    ? "flex-1 bg-sky-400 "
-                    : "flex-1 hover:bg-gray-400"
-            }
-                key="/"
-                to="/"
-                end>
-                <ScanBarcode/>
-            </NavLink>
-            <NavLink className={({ isActive }) =>
-                isActive
-                    ? "flex-1 bg-sky-400 "
-                    : "flex-1 hover:bg-gray-400"
-            }
-                key="/ajouter"
-                to="/ajouter"
-                end>
-                <PackagePlus />
-            </NavLink>
-        </div>
-    </>;
+const NavBar = () => {
+    return <Menubar className='fixed bottom-0 left-0 right-0 h-14 flex justify-around'>
+        <MenubarMenu>
+            <MenubarTrigger >
+                <NavLink className="flex-1"
+        key="/inventaire"
+        to="/inventaire"
+        end>
+        <Folder />
+    </NavLink></MenubarTrigger>
+        <MenubarTrigger ><NavLink className="flex-1"
+            key="/"
+            to="/"
+            end>
+            <ScanBarcode />
+        </NavLink></MenubarTrigger>
+        <MenubarTrigger ><NavLink className="flex-1"
+            key="/ajouter"
+            to="/ajouter"
+            end>
+            <PackagePlus />
+        </NavLink>
+        </MenubarTrigger>
+    </MenubarMenu>
+    </Menubar>;
 };
 
-export default Ajouter;
+export default NavBar;
