acc578e9eb7facae788825817f535aa05f5bbfa0
Enzo <aypierre30@gmail.com>
2025-06-02 17:07:25 +0200

    fix(style): scanner size

--- front-end/src/pages/scanner/Scanner.tsx
+++ front-end/src/pages/scanner/Scanner.tsx
@@ -2,7 +2,15 @@ import { BarcodeScanner } from "react-barcode-scanner";
 import "react-barcode-scanner/polyfill";
 
 export default function Scanner() {
-  return <BarcodeScanner options={{ formats: ['code_128'] }} onCapture={(result) => {
-    console.log(result);
-  }} />;
+  return (
+    <div className="fixed inset-0 w-screen h-screen overflow-hidden">
+      <BarcodeScanner
+        options={{ formats: ["code_128"] }}
+        onCapture={(result) => {
+          console.log(result);
+        }}
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
 }
