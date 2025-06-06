cb2761e63fd5e7391730e5b22ecbd73f31e30c57
Enzo <aypierre30@gmail.com>
2025-06-06 10:06:05 +0200

    fix(type): rangement des attributs dans le modèle article

--- back-end/app/Models/Article.ts
+++ back-end/app/Models/Article.ts
@@ -9,38 +9,12 @@ export default class Article extends BaseModel {
   @column({ isPrimary: true })
   public num_inventaire: string
 
-  @column({ serializeAs: null })
-  public categorie: number
-
-  @column({ serializeAs: null })
-  public id_piece: number
-
   @column()
   public num_serie: string
 
   @column()
   public num_bon_commande: string
 
-  @belongsTo(() => Piece, {
-    foreignKey: 'id_piece',
-    serializeAs: 'piece',
-  })
-  public piece: BelongsTo<typeof Piece>
-
-  @belongsTo(() => Categorie, {
-    foreignKey: 'categorie',
-    serializeAs: 'categorie',
-  })
-  public categorieRelation: BelongsTo<typeof Categorie>
-
-  @column()
-  public etat: number
-
-  @belongsTo(() => Etat, {
-    foreignKey: 'etat',
-    serializeAs: 'etat',
-  })
-  public etatRelation: BelongsTo<typeof Etat>
   @column()
   public fournisseur: string
 
@@ -50,4 +24,30 @@ export default class Article extends BaseModel {
   @column()
   public marque: string
 
+  @column({ serializeAs: null })
+  public categorie: number
+
+  @belongsTo(() => Categorie, {
+    foreignKey: 'categorie',
+    serializeAs: 'categorie',
+  })
+  public categorieRelation: BelongsTo<typeof Categorie>
+
+  @column({ serializeAs: null })
+  public id_piece: number
+
+  @belongsTo(() => Piece, {
+    foreignKey: 'id_piece',
+    serializeAs: 'piece',
+  })
+  public piece: BelongsTo<typeof Piece>
+
+  @column()
+  public etat: number
+
+  @belongsTo(() => Etat, {
+    foreignKey: 'etat',
+    serializeAs: 'etat',
+  })
+  public etatRelation: BelongsTo<typeof Etat>
 }
